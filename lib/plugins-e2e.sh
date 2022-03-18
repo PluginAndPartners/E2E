e68d50e7125c97f9720fb11c1cef4acd294f5d85
Maicon Gouveia <gouveia.maicon@gmail.com>
2022-03-18 20:00:17 -0300

    Add new conditions and file to install

---
+++ lib/plugins-e2e.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+E2E_PATH="/Users/mgouveia/Projects/E2E/"
+PLATAFORM=$1
+CYPRESS_PATH="${E2E_PATH}cypress/support/"
+
+if [ ! -z "$1" ]; then
+
+    echo "\n[MONTANDO AMBIENTE PARA PLATAFORMA ${PLATAFORM}]\n"
+
+    if [ ! -d ${CYPRESS_PATH} ]; then
+        echo "\n-- Criando pasta no Cypress --"    
+        mkdir ${CYPRESS_PATH}
+    else
+        echo "\n-- Remontando pasta no Cypress --" 
+        rm -r ${CYPRESS_PATH}
+        mkdir ${CYPRESS_PATH}
+    fi
+
+    echo "\n-- Copiando arquivo da plataforma (${PLATAFORM}) para a pasta do Cypress --"
+    cp -R "${E2E_PATH}/${PLATAFORM}/support/" ${CYPRESS_PATH}
+
+    echo "\n-- Rodando o Cypress"
+    npm run test:chrome
+
+else
+    echo "\n-- Execute o comando passando uma plataforma. (ex: woocommerce, magento, prestashop --"
+fi
