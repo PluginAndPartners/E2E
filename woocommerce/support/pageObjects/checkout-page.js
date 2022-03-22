f2ec7c66e48d5686b716d3e553499d6fdafdce45
sleaof <stephanie.leao@mercadolivre.com>
2022-03-22 09:58:16 -0300

    PPWP-914: remove characters from documents

--- woocommerce/support/pageObjects/checkout-page.js
+++ woocommerce/support/pageObjects/checkout-page.js
@@ -31,10 +31,6 @@ export const CheckoutPage = {
                     IframePage.getIframeElementName('iframe[name=securityCode]', '#securityCode').type(masterCard.cvv, {force: true});
                     IframePage.getIframeElementName('iframe[name=expirationDate]', '#expirationDate').type(masterCard.expirationDate, {force: true});
                     cy.get(CheckoutElements.paymentMethods.creditCard.nameInput).type(cardFullName, {force: true});
-                    cy.get(CheckoutElements.paymentMethods.creditCard.installmentsSelect).select('1', {force: true});
-                    cy.get(CheckoutElements.paymentMethods.creditCard.expirationDateInput).type(masterCard.expirationDate, {force: true});
-                    cy.get(CheckoutElements.paymentMethods.creditCard.cvvInput).type(masterCard.cvv, {force: true});
-                    cy.get(CheckoutElements.paymentMethods.creditCard.documentInput).type(userDocument.cpf, {force: true});
                     cy.get(CheckoutElements.paymentMethods.creditCard.installmentsSelect).click({force: true});
                     cy.get(CheckoutElements.paymentMethods.creditCard.documentInput).type(userDocument.cpf, {force: true});
                 });
